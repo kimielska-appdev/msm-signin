0f6ea0013e9d480823fc8beae840aaa73a99302a
kimielska <katie.imielska@chicagobooth.edu>
2022-05-20 17:55:48 +0000

    all done except some parts of bookmarks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   # Uncomment line 5 in this file and line 3 in UserAuthenticationController if you want to force users to sign in before any other actions.
   # before_action(:force_user_sign_in)
 
-
   def load_current_user
     the_id = session[:user_id]
     
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,16 @@
 class BookmarksController < ApplicationController
+
+  #before_action(:load_current_user) --> this will automatically call this method before any action is taken (do this instead of copying self.load_current_user repeatedly)
+
+  def load_current_user
+    @current_user = User.where({ :id => session.fetch(:user_id) }).at(0)
+  end
+
   def index
-    matching_bookmarks = Bookmark.all
+    #matching_bookmarks = Bookmark.where({:user_id => session.fetch(:user_id)})
+    @current_user = User.where({ :id => session.fetch(:user_id) }).at(0)
+
+    matching_bookmarks = @current_user.bookmarks
 
     @list_of_bookmarks = matching_bookmarks.order({ :created_at => :desc })
 
@@ -8,6 +18,8 @@ class BookmarksController < ApplicationController
   end
 
   def show
+    #@current_user = User.where({ :id => session.fetch(:user_id) }).at(0)
+    self.load_current_user
     the_id = params.fetch("path_id")
 
     matching_bookmarks = Bookmark.where({ :id => the_id })
@@ -18,19 +30,27 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    #@current_user = User.where({ :id => session.fetch(:user_id) }).at(0)
+    self.load_current_user
+
     the_bookmark = Bookmark.new
-    the_bookmark.user_id = params.fetch("query_user_id")
+    #the_bookmark.user_id = params.fetch("query_user_id")
+    the_bookmark.user_id = session.fetch(:user_id)
     the_bookmark.movie_id = params.fetch("query_movie_id")
 
     if the_bookmark.valid?
       the_bookmark.save
-      redirect_to("/bookmarks", { :notice => "Bookmark created successfully." })
+      #redirect_to("/bookmarks", { :notice => "Bookmark created successfully." })
+      redirect_to("/movies/#{the_bookmark.movie_id}", { :notice => "Bookmark created successfully." })
     else
       redirect_to("/bookmarks", { :alert => the_bookmark.errors.full_messages.to_sentence })
     end
   end
 
   def update
+    #@current_user = User.where({ :id => session.fetch(:user_id) }).at(0)
+    self.load_current_user
+
     the_id = params.fetch("path_id")
     the_bookmark = Bookmark.where({ :id => the_id }).at(0)
 
@@ -39,18 +59,21 @@ class BookmarksController < ApplicationController
 
     if the_bookmark.valid?
       the_bookmark.save
-      redirect_to("/bookmarks/#{the_bookmark.id}", { :notice => "Bookmark updated successfully."} )
+      redirect_to("/bookmarks/#{the_bookmark.id}", { :notice => "Bookmark updated successfully." })
     else
       redirect_to("/bookmarks/#{the_bookmark.id}", { :alert => the_bookmark.errors.full_messages.to_sentence })
     end
   end
 
   def destroy
+    #@current_user = User.where({ :id => session.fetch(:user_id) }).at(0)
+    self.load_current_user
+
     the_id = params.fetch("path_id")
     the_bookmark = Bookmark.where({ :id => the_id }).at(0)
 
     the_bookmark.destroy
 
-    redirect_to("/bookmarks", { :notice => "Bookmark deleted successfully."} )
+    redirect_to("/bookmarks", { :notice => "Bookmark deleted successfully." })
   end
 end
