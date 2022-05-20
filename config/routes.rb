5830cf1cd63c74d63c295f5f145994bc76aefcf8
kimielska <katie.imielska@chicagobooth.edu>
2022-05-20 16:58:32 +0000

    added bookmark form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/add_bookmark",{:controller =>"movies", :action=>"add_user_bookmark"})
+
   get("/", {:controller => "user_authentication", :action=>"index"})
 
   # Routes for the Bookmark resource:
